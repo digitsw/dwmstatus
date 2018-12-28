3ebf4475715c1e723e6c0c23c7ba491f8475e9c5
Akram Chehaima <chakramabdeslem@gmail.com>
2018-12-28 04:28:04 +0100

    get ALSA Volume & Battery

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 dwmstatus: status.c
-	cc -o dwmstatus status.c -lX11
+	cc -o dwmstatus status.c -lX11 `pkg-config --cflags --libs alsa`
 clean: rm dwmstatus
--- status.c
+++ status.c
@@ -1,42 +1,19 @@
-#define _BSD_SOURCE
-#include <unistd.h>
-#include <stdbool.h>
+#define BATT_NOW        "/sys/class/power_supply/BAT0/charge_now"
+#define BATT_FULL       "/sys/class/power_supply/BAT0/charge_full"
+#define BATT_STATUS       "/sys/class/power_supply/BAT0/status"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <string.h>
-#include <strings.h>
-#include <sys/time.h>
 #include <time.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+
+#include <alsa/asoundlib.h>
 
 #include <X11/Xlib.h>
 
-static Display *dpy;
-
-char *
-smprintf(char *fmt, ...)
+static void die(const char *errmsg)
 {
-    va_list fmtargs;
-    char *ret;
-    int len;
-
-    va_start(fmtargs, fmt);
-    len = vsnprintf(NULL, 0, fmt, fmtargs);
-    va_end(fmtargs);
-
-    ret = malloc(++len);
-    if (ret == NULL) {
-        perror("malloc");
-        exit(1);
-    }
-
-    va_start(fmtargs, fmt);
-    vsnprintf(ret, len, fmt, fmtargs);
-    va_end(fmtargs);
-
-    return ret;
+    fputs(errmsg, stderr);
+    exit(EXIT_FAILURE);
 }
 int
 parse_netdev(unsigned long long int *receivedabs, unsigned long long int *sentabs)
@@ -111,35 +88,122 @@ get_netusage(unsigned long long int *rec, unsigned long long int *sent)
     return retstr;
 }
 
-void
-setstatus(char *str)
+static snd_mixer_t *alsainit(const char *card)
 {
-    XStoreName(dpy, DefaultRootWindow(dpy), str);
-    XSync(dpy, False);
+    snd_mixer_t *handle;
+
+    snd_mixer_open(&handle, 0);
+    snd_mixer_attach(handle, card);
+    snd_mixer_selem_register(handle, NULL, NULL);
+    snd_mixer_load(handle);
+    return handle;
 }
-int
-main(void)
+
+static void alsaclose(snd_mixer_t *handle)
 {
-    char *status;
+    snd_mixer_close(handle);
+}
+
+static snd_mixer_elem_t *alsamixer(snd_mixer_t *handle, const char *mixer)
+{
+    snd_mixer_selem_id_t *sid;
+
+    snd_mixer_selem_id_alloca(&sid);
+    snd_mixer_selem_id_set_index(sid, 0);
+    snd_mixer_selem_id_set_name(sid, mixer);
+    return snd_mixer_find_selem(handle, sid);
+}
+
+static int ismuted(snd_mixer_elem_t *mixer)
+{
+    int on;
+
+    snd_mixer_selem_get_playback_switch(mixer, SND_MIXER_SCHN_MONO, &on);
+    return !on;
+}
+
+static int getvol(snd_mixer_elem_t *mixer)
+{
+    long vol, min, max;
+
+    snd_mixer_selem_get_playback_volume_range(mixer, &min, &max);
+    snd_mixer_selem_get_playback_volume(mixer, SND_MIXER_SCHN_MONO, &vol);
+
+    vol = vol < max ? (vol > min ? vol : min) : max;
+    return vol * 100.0 / max + 0.5;
+}
+
+
+static char *gettime()
+{
+    static char timestr[60];
+    time_t rawtime;
+
+    time(&rawtime);
+    strftime(timestr, sizeof(timestr), "%d %B %H:%M", localtime(&rawtime));
+    return timestr;
+}
+
+int get_power()   
+{
+    FILE *fp = NULL;
+    long lnum1, lnum2 = 0;
+    if ((fp = fopen(BATT_NOW, "r"))) {
+        fscanf(fp, "%ld\n", &lnum1);
+        fclose(fp);
+        fp = fopen(BATT_FULL, "r");
+        fscanf(fp, "%ld\n", &lnum2);
+        fclose(fp);
+    }
+    return (lnum1 /(lnum2/100)); 
+}
+
+char *
+getbat_status(){
+    FILE *fp = NULL;
+    char *status = malloc(sizeof(char)*12);
+    fp = fopen(BATT_STATUS, "r");
+    fscanf(fp, "%s\n", status);
+    fclose(fp);
+    if (strcmp(status,"Charging") == 0)
+                 return "+";
+             if (strcmp(status,"Discharging") == 0)
+                 return "-";
+             if (strcmp(status,"Full") == 0)
+                 return "=";
+             else
+                 return "n/a";
+}
+
+int
+main(void) {
+    snd_mixer_t *alsa;
+    snd_mixer_elem_t *mixer;
+    char buf[100];
+    static Display *dpy;
     char *netstats;
     static unsigned long long int rec, sent;
-
-    if (!(dpy = XOpenDisplay(NULL))) {
-        fprintf(stderr, "dwmstatus: cannot open display.\n");
-        return 1;
-    }
+    if (!(dpy = XOpenDisplay(NULL)))
+        die("dstatus: cannot open display \n");
+    if (!(alsa = alsainit("default")))
+        die("dstatus: cannot initialize alsa\n");
+    if (!(mixer = alsamixer(alsa, "Master")))
+        die("dstatus: cannot get mixer\n");
 
     parse_netdev(&rec, &sent);
-    for (;;sleep(1)) {
-        netstats = get_netusage(&rec, &sent);
+    while(1)
+        {
+            
+            netstats = get_netusage(&rec, &sent);
+            snprintf(buf, sizeof(buf), "|N:%s|B:%d%s|V:%d%%%s|%s", netstats ,
+                     get_power(),getbat_status(),getvol(mixer), ismuted(mixer) ? " [off]" : "",gettime());
 
-        status = smprintf("|N: %s|",
-                          netstats);
-        setstatus(status);
-        free(status);
-    }
-
+            XStoreName(dpy, DefaultRootWindow(dpy), buf);
+            XSync(dpy, False);
+            snd_mixer_wait(alsa, 10000);
+            snd_mixer_handle_events(alsa);
+        }
+    alsaclose(alsa);
     XCloseDisplay(dpy);
-
     return 0;
 }
